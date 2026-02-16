eb5cd26c0eff3ce003aec630ae6ec36f626d6e0c
steve12345585 <steve12345585@users.noreply.github.com>
2026-02-16 19:41:26 +0000

    Branding updates: truncate 'libre' for better code searchability

--- unodevtools/source/skeletonmaker/skeletoncommon.cxx
+++ unodevtools/source/skeletonmaker/skeletoncommon.cxx
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- * This file is part of the LibreOffice project.
+ * This file is part of the SnipeOffice project.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -40,7 +40,7 @@ void printLicenseHeader(std::ostream& o)
 {
     o << "/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */\n"
          "/*\n"
-         " * This file is part of the LibreOffice project.\n"
+         " * This file is part of the SnipeOffice project.\n"
          " *\n"
          " * This Source Code Form is subject to the terms of the Mozilla Public\n"
          " * License, v. 2.0. If a copy of the MPL was not distributed with this\n"
